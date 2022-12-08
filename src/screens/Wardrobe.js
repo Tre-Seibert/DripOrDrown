11db2030c04ed35c09968c2245ba106c4be3e4d3
srock01 <seanrock0803@gmail.com>
2022-12-08 18:51:08 -0500

    firebase config update

--- src/screens/Wardrobe.js
+++ src/screens/Wardrobe.js
@@ -1,18 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Image, View, Platform, Text, StyleSheet, TouchableOpacity, SafeAreaView, Alert} from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import firebaseConfig from '/Users/treseibert/Documents/Courses/Software-Engineering/DripOrDrown/DripOrDrown/firebaseConfig.js';
 import { initializeApp } from 'firebase/app';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 export var url;
 export var theURL;
 global.Buffer = global.Buffer || require('buffer').Buffer
-
-initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyAF9QW9bvXKyWIiPpmaOgKunA51Jxe4iAw",
+  authDomain: "dripordrown-90905.firebaseapp.com",
+  databaseURL: "https://dripordrown-90905-default-rtdb.firebaseio.com",
+  projectId: "dripordrown-90905",
+  storageBucket: "dripordrown-90905.appspot.com",
+  messagingSenderId: "217796469697",
+  appId: "1:217796469697:web:3324196fa615c8c4f6c540",
+  measurementId: "G-F0RSLNR2DY",
+};
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
 
 
 export default function ImagePickerExample() {
-  
+  async function crop(theURL) {
+    const formData = new FormData();
+    formData.append('size', 'auto');
+    formData.append('image_url', theURL);
+    
+    axios({
+    method: 'post',
+    url: 'https://api.remove.bg/v1.0/removebg',
+    data: formData,
+    responseType: 'arraybuffer',
+    headers: data.getHeaders ? data.getHeaders() : { 'Content-Type': 'multipart/form-data' },
+    headers: {
+        ...formData.getHeaders(),
+        'X-Api-Key': 'kLJT2Gev5QbZe5epeexwrrKn',
+    },
+    encoding: null
+    })
+    .then((response) => {
+    if(response.status != 200) return console.error('Error:', response.status, response.statusText);
+    fs.writeFileSync("no-bg.png", response.data);
+    })
+    .catch((error) => {
+        return console.error('Request failed:', error);
+    });
+  }
   const [image, setImage] = useState(null);
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -28,11 +64,11 @@ export default function ImagePickerExample() {
     if (!result.canceled) {
       setImage(result.assets[0].uri);
     }
-    var inputPath = {uri: result.uri};
-    const storage = getStorage();
+    var inputPath = {uri: result.assets[0].uri};
+    
     const reference = ref(storage, 'image.jpg');
 
-    const img = await fetch(result.uri);
+    const img = await fetch(result.assets[0].uri);
     const bytes = await img.blob();
 
     uploadBytes(reference, bytes);
@@ -47,6 +83,7 @@ export default function ImagePickerExample() {
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
       {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <Button title="Pick an image from camera roll" onPress={() => crop(image)} />
     </View>
   );
 }
--- src/screens/removeback.js
+++ src/screens/removeback.js
@@ -1,4 +1,4 @@
-import { theURL } from './Wardrobe';
+//import { theURL } from './Wardrobe';
 const axios = require('axios');
 const FormData = require('form-data');
 const fs = require('fs');
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 const formData = new FormData();
 formData.append('size', 'auto');
-formData.append('image_url', theURL);
+formData.append('image_url', 'https://firebasestorage.googleapis.com/v0/b/dripordrown-90905.appspot.com/o/image.jpg?alt=media&token=2d0b634e-17cf-4d19-acaa-0f26484cef6d');
 
 axios({
 method: 'post',
