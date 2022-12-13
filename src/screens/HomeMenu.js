4faf6ebde6c8c79ef5d94652e1fe9c2cd26b918e
J-Salt <js730674@sju.edu>
2022-12-13 12:21:04 -0500

    switched from using page stack to bottom tab nav

--- src/screens/HomeMenu.js
+++ src/screens/HomeMenu.js
@@ -1,48 +1,35 @@
-import React from 'react';
-import { Button, View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 import { initializeApp } from "firebase/app";
-import {
-  getDocs,
-  getFirestore,
-  collection,
-  firestore,
-  firebase,
-  addDoc,
-  get,
-  doc,
-  getDoc,
-  setDoc,
-  getDocFromCache
-} from "firebase/firestore/lite";
-import { color } from 'react-native-reanimated';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { getFirestore } from "firebase/firestore/lite";
 const firebaseConfig = {
-  apiKey: "AIzaSyAF9QW9bvXKyWIiPpmaOgKunA51Jxe4iAw",
-  authDomain: "dripordrown-90905.firebaseapp.com",
-  databaseURL: "https://dripordrown-90905-default-rtdb.firebaseio.com",
-  projectId: "dripordrown-90905",
-  storageBucket: "dripordrown-90905.appspot.com",
-  messagingSenderId: "217796469697",
-  appId: "1:217796469697:web:3324196fa615c8c4f6c540",
-  measurementId: "G-F0RSLNR2DY",
+    apiKey: "AIzaSyAF9QW9bvXKyWIiPpmaOgKunA51Jxe4iAw",
+    authDomain: "dripordrown-90905.firebaseapp.com",
+    databaseURL: "https://dripordrown-90905-default-rtdb.firebaseio.com",
+    projectId: "dripordrown-90905",
+    storageBucket: "dripordrown-90905.appspot.com",
+    messagingSenderId: "217796469697",
+    appId: "1:217796469697:web:3324196fa615c8c4f6c540",
+    measurementId: "G-F0RSLNR2DY",
 };
-
+import Closet from "./Closet";
+import CalendarPage from "./Calendar";
 require("firebase/firestore");
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const Tab = createBottomTabNavigator();
 
-export default function Home ({navigation, route}) {
-  const { email } = route.params;
-  
+export default function Home() {
     return (
-      <View>
-        <Text>Home Screen </Text>
-
-        <Button
-          title="Closet"
-          color="blue"
-          onPress={() => navigation.navigate('Closet',{ email: email })}
-        />
-      </View>
+        <Tab.Navigator initialRouteName="Calendar">
+            <Tab.Screen
+                name="Closet"
+                component={Closet}
+                options={{ headerShown: false }}
+            />
+            <Tab.Screen name="Calendar" component={CalendarPage} />
+        </Tab.Navigator>
     );
-  }
+}
