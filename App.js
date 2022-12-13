538a0e4c6d123f3ebc167edeb3851cd6bd0e4298
J-Salt <js730674@sju.edu>
2022-12-13 12:17:11 -0500

    removed clutter and certain pages

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
-//import {firebase, db} from './firebase/firebase';
 import { StyleSheet, Text, useColorScheme, View } from "react-native";
 
 import { Colors } from "react-native/Libraries/NewAppScreen";
 
-//import Clothes from "./src/component/Clothes";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import HomeMenu from "./src/screens/HomeMenu";
-import Closet from "./src/screens/Closet";
 import LoadIn from "./src/screens/LoadIn";
 import LogIn from "./src/screens/LogIn";
 import SignUp from "./src/screens/SignUp";
@@ -34,36 +32,8 @@ const firebaseConfig = {
 const fbApp = initializeApp(firebaseConfig);
 const db = getFirestore(fbApp);
 const Stack = createStackNavigator();
-
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
-const Section = ({ children, title }) => {
-    const isDarkMode = useColorScheme() === "dark";
-    return (
-        <View style={styles.sectionContainer}>
-            <Text
-                style={[
-                    styles.sectionTitle,
-                    {
-                        color: isDarkMode ? Colors.white : Colors.black,
-                    },
-                ]}
-            >
-                {title}
-            </Text>
-            <Text
-                style={[
-                    styles.sectionDescription,
-                    {
-                        color: isDarkMode ? Colors.light : Colors.dark,
-                    },
-                ]}
-            >
-                {children}
-            </Text>
-        </View>
-    );
-};
 
 const App = () => {
     const isDarkMode = useColorScheme() === "dark";
@@ -73,7 +43,6 @@ const App = () => {
     };
 
     const [checking, setIsChecking] = React.useState(true);
-    //console.log(users);
 
     return (
         <NavigationContainer>
@@ -90,8 +59,6 @@ const App = () => {
                 />
                 <Stack.Screen name="Login" component={LogIn} />
                 <Stack.Screen name="Register" component={SignUp} />
-                <Stack.Screen name="Home" component={HomeMenu} />
-                <Stack.Screen name="Closet" component={Closet} />
                 <Stack.Screen name="Article" component={Article} />
                 <Stack.Screen
                     name="ForgotPassword"
@@ -99,6 +66,7 @@ const App = () => {
                 />
                 <Stack.Screen name="AddArticle" component={AddArticle} />
                 <Stack.Screen name="Calendar" component={CalendarScreen} />
+                <Stack.Screen name="Home" component={HomeMenu}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
